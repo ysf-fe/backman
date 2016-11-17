c9bd945dbf0e9adf367447e0ed95f59da81dfb27
TevinLi <6317595@qq.com>
2016-11-17 14:03:14 +0800

    优化峰驼转下划线

--- build/backman/backman.js
+++ build/backman/backman.js
@@ -261,25 +261,37 @@ backman.constant('_tools', (function () {
             for (var p in json) {
                 if (json.hasOwnProperty(p)) {
                     var key;
-                    if (typeof p == 'string') {
+                    //字符串进行键名转换
+                    if (!/^\d+$/.test(p)) {
                         if (type == 'camel') {
                             key = toCamel(p);
                         } else if (type == 'underline') {
                             key = toUnderline(p);
                         }
-                    } else {
-                        key = p;
                     }
+                    //数值直接传递
+                    else {
+                        key = parseInt(p);
+                    }
+                    //属性为对象时，递归转换
                     if (json[p] instanceof Object) {
-                        json2[key] = transform(json[p], typeof json[p].length == 'undefined' ? {} : []);
-                    } else {
+                        json2[key] = transform(json[p], $.type(json[p]) == 'array' ? [] : {});
+                    }
+                    //属性非对象，为字符串但内容符合json格式，递归转换
+                    else if ($.type(json[p]) == 'string' && /^[\{\[]+("([a-zA-Z][a-zA-Z0-9\-_]*?)"\:(.+?))+[\}\]]+$/.test(json[p])) {
+                        json2[key] = JSON.parse(json[p]);
+                        json2[key] = transform(json2[key], $.type(json2[key]) == 'array' ? [] : {});
+                        json2[key] = JSON.stringify(json2[key]);
+                    }
+                    //属性非对象，非json字符串，直接传递
+                    else {
                         json2[key] = json[p];
                     }
                 }
             }
             return json2;
         };
-        return transform(json, typeof json.length == 'undefined' ? {} : []);
+        return transform(json, $.type(json) == 'array' ? [] : {});
     };
 
     var loadJs = function (jsList) {
